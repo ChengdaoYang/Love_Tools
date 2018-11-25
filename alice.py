bb97239a929b5d41ffcdd449c3edb4cea5c9b678
wansi xie <wx2210@columbia.edu>
2018-11-25 17:14:06 +0000

    Second website

--- alice.py
+++ alice.py
@@ -1,20 +1,72 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-url = 'https://www.thehindubusinessline.com/'
-response = requests.get(url)
-containers = BeautifulSoup(response.content,'lxml')
-data = []
-for container in containers.find_all("h4"):
-    container_title = container.text
-    container_link = container.a['href']
+# website 15
+def website15():
+    url = 'https://www.thehindubusinessline.com/'
+    response = requests.get(url)
+    containers = BeautifulSoup(response.content,'lxml')
+    data = []
+    for container in containers.find_all("h4"):
+        container_title = container.text
+        container_link = container.a['href']
 
-    response = requests.get(container_link)
-    page = BeautifulSoup(response.content,'lxml')
-    paragraphs = page.find_all("p")
-    text = ""
-    for paragraph in paragraphs:
-        text += paragraph.text
-           
-    data.append((container_title,container_link,text))
-print(data)
+        response = requests.get(container_link)
+        page = BeautifulSoup(response.content,'lxml')
+        paragraphs = page.find_all("p")
+        text = ""
+        for paragraph in paragraphs:
+            text += paragraph.text
+
+        data.append((container_title,container_link,text))
+
+    return(data)
+# website 7
+def website7():
+    url = "https://www.morningstar.com/"
+    response = requests.get(url)
+    containers = BeautifulSoup(response.content,'lxml')
+    response.status_code
+    data = []
+    for abc in containers.find_all("div",{"class":"article-lineup-item section"}):
+
+        if abc.a['href'][0] == '/':
+            container_link = 'https://www.morningstar.com/' + abc.a['href']
+            match = re.search(r'\d/(.+?).html',container_link)
+            container_title = match.groups()[0]
+
+            response = requests.get(container_link)
+            page = BeautifulSoup(response.content,'lxml')
+            paragraphs = page.find_all("p")
+            text = ""
+            for paragraph in paragraphs:
+                text += paragraph.text
+
+            data.append((container_title,container_link,text))
+
+    return(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
