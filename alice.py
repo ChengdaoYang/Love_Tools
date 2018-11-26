b321783efd370ea61bb8f423820be6864c222c35
wansi xie <wx2210@columbia.edu>
2018-11-26 00:13:24 +0000

    Analyze emotions

--- alice.py
+++ alice.py
@@ -46,7 +46,7 @@ def website7():
 
     return(data)
 # website 19
-def website19()
+def website19():
     data = []
     for container in containers.find_all("article",{"class":"topnews__story"}):
         container_title = container.text
@@ -66,6 +66,52 @@ def website19()
 
 
 
+
+# analyzes emotions
+def get_pos_neg_words():
+    def get_words(url):
+        import requests
+        words = requests.get(url).content.decode('latin-1')
+        word_list = words.split('\n')
+        index = 0
+        while index < len(word_list):
+            word = word_list[index]
+            if ';' in word or not word:
+                word_list.pop(index)
+            else:
+                index+=1
+        return word_list
+
+    #Get lists of positive and negative words
+    p_url = 'http://ptrckprry.com/course/ssd/data/positive-words.txt'
+    n_url = 'http://ptrckprry.com/course/ssd/data/negative-words.txt'
+    positive_words = get_words(p_url)
+    negative_words = get_words(n_url)
+    return positive_words,negative_words
+
+positive_words,negative_words = get_pos_neg_words()
+
+def analyze_emotions(result):
+    data =[]
+    for i in range(len(result)):
+        text = result[i]
+    #     print(text[0])
+
+        number = len(word_tokenize(text[2]))
+        pos = neg = 0
+        for word in word_tokenize(text[2]):
+            if word in positive_words:
+                pos+=1
+            if word in negative_words:
+                neg+=1
+
+        data.append([text[0],pos/number*100,neg/number*100])
+
+    data = pd.DataFrame(data,columns = ['title','positive%','negative%']) 
+
+    return data
+
+import pandas as pd
 
 
 
