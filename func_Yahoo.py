50f50cad52561e1c8ded1a3666c73e266a8a493d
Chengdao Yang <chengdaoyang@live.com>
2018-12-01 17:27:06 -0500

    add debug mode to func_Yahoo and edit main

--- func_Yahoo.py
+++ func_Yahoo.py
@@ -2,39 +2,69 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import json
+import time
+import random
 
-def get_Yahoo(ticker, out_put=False):
+def get_Yahoo(ticker, out_put=False, debug=False):
 
     base_url = 'https://finance.yahoo.com'
     url = f"https://finance.yahoo.com/quote/{ticker}/news?p={ticker}"
 
-    response = requests.get(url)
-    Soup = BeautifulSoup(response.content,'lxml')
+    try:
+        #request the url, Soup parse and get tag of each relavent article
+        response = requests.get(url)
+        if debug:
+            print(response)
 
-    All_tags = Soup.find_all('a',
-                            {'class':
-                            'Fw(b) Fz(20px) Lh(23px) LineClamp(2,46px) Fz(17px)'
-                            +'--sm1024 Lh(19px)--sm1024 LineClamp(2,38px)'
-                            +'--sm1024 Td(n) C(#0078ff):h C(#000)'})
-    
-    url_list = []
-    for tag in All_tags:
-        url_list.append(tag.get('href'))
-    url_list   
+        if response.status_code == 200:
+            if debug:
+                print(response)
+            Soup = BeautifulSoup(response.content,'lxml')
+        else:
+            return 'error'
+        All_tags = Soup.find_all('a',
+                                {'class':
+                                'Fw(b) Fz(20px) Lh(23px) LineClamp(2,46px) Fz(17px)'
+                                +'--sm1024 Lh(19px)--sm1024 LineClamp(2,38px)'
+                                +'--sm1024 Td(n) C(#0078ff):h C(#000)'})
+        if debug:
+            print(All_tags)
 
-    text = ''
-    for url in url_list:
-        response = requests.get(base_url + url)
-        Soup = BeautifulSoup(response.content, 'lxml')
-        All_tags = Soup.find_all('p', {'class':'canvas-atom canvas-text Mb(1.0em) Mb(0)--sm Mt(0.8em)--sm'})
-    
+        #get all article links from a tags and put into a list
+        url_list = []
         for tag in All_tags:
-            text = text + (tag.get_text())
+            if debug:
+                print(tag.get('href'))
+            url_list.append(tag.get('href'))
 
+
+        if debug:
+            print(url_list)
+            print('start getting article from url_lists')
+        #get text from each article url and put into one big lonf string
+        text = ''
+        try:
+            for link in url_list:
+                time.sleep(random.random())
+                if debug:
+                    print(base_url + link)
+                response = requests.get(base_url + link)
+                Soup = BeautifulSoup(response.content, 'lxml')
+                All_tags = Soup.find_all('p', {'class':'canvas-atom canvas-text Mb(1.0em) Mb(0)--sm Mt(0.8em)--sm'})
+            
+                if debug:
+                    print('getting text from:', base_url+link)
+                for tag in All_tags:
+                    text = text + (tag.get_text())
+        except:
+            print('error')
+
+    except:
+        print('error')
     if out_put:
         with open(f'{ticker}_result.txt', 'w') as fp:
             fp.write(text)
 
     return text
 
-#print(get_Yahoo('AAPL',out_put=True))
+#print(get_Yahoo('AAPL',out_put=True, debug=True))
