4e3c3c13bc854e8a9e4317995434696251f83642
wansi xie <wx2210@columbia.edu>
2018-12-01 21:58:21 +0000

    revise eight emotions

--- func_Eight_emotion.py
+++ func_Eight_emotion.py
@@ -1,42 +1,38 @@
 
-def get_Emotion_data():
-    # Get NRC sentiment data and save to text 
-    # Return a text name
-    url = "https://raw.githubusercontent.com/sebastianruder/emotion_proposition_store/master/NRC-Emotion-Lexicon-v0.92/NRC-emotion-lexicon-wordlevel-alphabetized-v0.92.txt"
-    words = requests.get(url).content.decode('latin-1')
-    nrc = 'data/emotion_words.txt'
-    with open(nrc,'w') as fp:
-        fp.write(words) 
-    return nrc
-
-nrc = get_emotion_data() # emotion_words.txt name
-
-
-def get_Eight_emotion(name,nrc):
-
-#Input stock name and emotion_words.txt name
+def get_Eight_emotion(text):
+#Input stock news --> string
 #Output 8 emotions
 #NRC data codifies words with emotions
-#14,182 words are coded into 2 sentiments and 8 emotions
+#14,182 words are coded into 2 sentiments and 8 emotions    
+    def get_Emotion_data():
+        # Get NRC sentiment data and save to text 
+        # Return a text name
+        url = '''https://raw.githubusercontent.com/sebastianruder/emotion_proposition_store/
+         master/NRC-Emotion-Lexicon-v0.92/NRC-emotion-lexicon-wordlevel-alphabetized-v0.92.txt'''
+        words = requests.get(url).content.decode('latin-1')
+        nrc = 'emotion_words.txt'        
+        with open(nrc,'w') as fp:
+            fp.write(words) 
+        pass  
 
-    def get_nrc_data(nrc):
+    def get_NRC_data():
         # Read text and return emotion words
         count=0
         emotion_dict=dict()
-        with open(nrc,'r') as f:
+        with open("emotion_words.txt","r") as f:
             for line in f:
-                while count >=47:
-                    line = line.strip().split('\t')
-                    if int(line[2]) == 1:
-                        if emotion_dict.get(line[0]):
-                            emotion_dict[line[0]].append(line[1])
-                        else:
-                            emotion_dict[line[0]] = [line[1]]
+                if count < 46:
+                    count+=1
+                    continue
+                line = line.strip().split('\t')
+                if int(line[2]) == 1:
+                    if emotion_dict.get(line[0]):
+                        emotion_dict[line[0]].append(line[1])
+                    else:
+                        emotion_dict[line[0]] = [line[1]]
         return emotion_dict
-    
-    emotion_dict = get_nrc_data(nrc)
 
-    def emotion_analyzer(text,emotion_dict=emotion_dict):
+    def get_Emotion_analyzer(text,emotion_dict):
         #Set up the result dictionary
         emotions = {x for y in emotion_dict.values() for x in y}
         emotion_count = dict()
@@ -50,16 +46,18 @@ def get_Eight_emotion(name,nrc):
                     emotion_count[emotion] += 1/len(text.split())
         return emotion_count
     
-    result = emotion_analyzer(text)
+        
+    try:
+        open('emotion_words.txt','r')
+    except FileNotFoundError:
+        get_Emotion_data() 
+        
+    emotion_dict = get_NRC_data()        
+    result = get_Emotion_analyzer(text,emotion_dict)
     
-    return ("%-12s %1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f"%(
-        name,result['fear'],result['trust'],
+    return ("%1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f\t%1.2f"%(
+        result['fear'],result['trust'],
           result['negative'],result['positive'],result['joy'],result['disgust'],
           result['anticipation'],result['sadness'],result['surprise']))
 
-
-with open("AAPL2.txt",'r', encoding="utf-8") as fp:
-        text = fp.read()  
-
-name = 'AAPL'
-print(eight_emotion(name,nrc))
+#get_Eight_emotion(text)
