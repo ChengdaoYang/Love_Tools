acb164d25293da090e140b6018eb57f3738d15a1
wansi xie <wx2210@columbia.edu>
2018-12-02 00:59:50 +0000

    add try

--- func_Bloomberg.py
+++ func_Bloomberg.py
@@ -1,27 +1,45 @@
-def get_Bloomberg(name, date_, out_put = False):
+# get the text from bloomberg and return news 
+def get_Bloomberg(name, day, out_put = False, debug=False):
     
     url = "https://www.bloombergquint.com/search?q=" + name
-    response = requests.get(url)
-    containers = BeautifulSoup(response.content,'lxml')
+    try:
+    #request the url, Soup parse and get tag of each relavent article
+        response = requests.get(url)  
+        containers = BeautifulSoup(response.content,'lxml')
+        if response.status_code != 200:
+            return None
     
-    text = ""
-    for container in containers.find_all("li",{"class":"topic-page__item"}):
-
-        link = "https://www.bloombergquint.com" + container.h3.a["href"]
-        t = container.time["datetime"]
-        delta = round((time.time() - float(t[:-3]))/86400,2)
-        if delta > date_:
-            break
+        text = ""
+        for container in containers.find_all("li",{"class":"topic-page__item"}):
+    
+            link = "https://www.bloombergquint.com" + container.h3.a["href"]
+            t = container.time["datetime"]
+            delta = round((time.time() - float(t[:-3]))/86400,2)
+            if delta > day:
+                if debug:
+                    print('delta break')
+                break
+            
+            try: 
+                response = requests.get(link)
+                if response.status_code != 200:
+                    if debug:
+                        print('error get paragraph')
+                    break
+                page = BeautifulSoup(response.content,'lxml')
+                paragraphs = page.find_all("p")
         
-        response = requests.get(link)
-        page = BeautifulSoup(response.content,'lxml')
-        paragraphs = page.find_all("p")
-
-        for paragraph in paragraphs:
-            text += paragraph.text
-            
-    if out_put:
-        with open(f'{name}_bloomberg.txt', 'w', encoding="utf-8") as fp:
-            fp.write(text)
-            
-    return text
+                for paragraph in paragraphs:
+                    text += paragraph.text
+            except:
+                break               
+        if out_put:
+            with open(f'{name}_bloomberg.txt', 'w', encoding="utf-8") as fp:
+                fp.write(text)
+                
+        return text
+    except:  
+        return None
+    
+#a = get_Bloomberg('Apple', day = 30, out_put = False)
+#print(a)
