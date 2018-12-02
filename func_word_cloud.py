fd9fe2bebbdb878ca75163acce01c4ca57c88846
Manchun Sun <sunmanchun@outlook.com>
2018-12-02 13:44:55 -0500

    improve word_cloud

--- func_word_cloud.py
+++ func_word_cloud.py
@@ -3,16 +3,11 @@
 
 from wordcloud import WordCloud,STOPWORDS
 import matplotlib.pyplot as plt
-def get_word_cloud_picture(filename):
-    f = open(filename,'r')
-    text = f.read()
+def get_word_cloud_picture(text, output_name):
     wordcloud = WordCloud(stopwords=STOPWORDS,background_color='white',width=3000,height=2500,max_words=50).generate(text)
     plt.figure(1,figsize=(13, 13))
+    plt.imshow(wordcloud)
     plt.axis('off')
     plt.title('Wordcloud picture')
-    plt.savefig('wordcloud.png')
-    print(type('wordcloud'))
+    plt.savefig(f'{output_name}.png')
     return wordcloud
-
-plt.imshow(get_word_cloud_picture('Amazon_filtered.txt'))
-
