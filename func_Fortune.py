d3d270506b95b614faf953b1982dd84241e4f33b
Xiangdong Duan <trihesdlin@163.com>
2018-12-01 19:14:26 -0500

    Update func_Fortune.py.

--- func_Fortune.py
+++ func_Fortune.py
@@ -16,27 +16,32 @@ def get_Fortune(keyword , date, out_put = False):
         return base_url
 
     def find_in_one_page(url,j):
-        response = requests.get(url)
-        results_page = BeautifulSoup(response.content)
-        all_div_tags = results_page.find_all('div',{'class':'headline heading-content-tiny margin-8-bottom media-heading'})
-        data = []
-        for tag in all_div_tags:
-            # print(tag)
-            a_tag = tag.find('a')
-            data.append(a_tag.get('href'))
-        selected_links = []
-        for page in data:
-            date_if = page[19:29]
-            try:
-                d = datetime.datetime.strptime(date_if, '%Y/%m/%d')
-                if d > old_day:
-                    selected_links.append(page)
-                else:
-                    j = -100
-                    break
-            except:
-                continue
-        return selected_links,j
+        try:
+            response = requests.get(url)
+            results_page = BeautifulSoup(response.content)
+            all_div_tags = results_page.find_all('div',{'class':'headline heading-content-tiny margin-8-bottom media-heading'})
+            data = []
+            for tag in all_div_tags:
+                # print(tag)
+                a_tag = tag.find('a')
+                data.append(a_tag.get('href'))
+            selected_links = []
+            for page in data:
+                date_if = page[19:29]
+                try:
+                    d = datetime.datetime.strptime(date_if, '%Y/%m/%d')
+                    if d > old_day:
+                        selected_links.append(page)
+                    else:
+                        j = -100
+                        break
+                except:
+                    continue
+            return selected_links, j
+        except:
+            j = -100
+            return [],j
+
 
     old_day = get_old_day()
     j = 1
@@ -46,6 +51,9 @@ def get_Fortune(keyword , date, out_put = False):
         selected_links,j = find_in_one_page(url,j)
         all_links.extend(selected_links)
         j = j + 1
+
+    if all_links == []:
+        return None
 
     def get_content(url):
         response = requests.get(url)
